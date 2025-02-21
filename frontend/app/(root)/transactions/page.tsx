7733934ebbbaea9481a3b1fe2473a8c04c575069
Colin Billingsley <129898391+colinbillingsley@users.noreply.github.com>
2025-02-21 16:09:51 -0600

    added type filter. made filters responsive. added filter icon for mobile.

--- frontend/app/(root)/transactions/page.tsx
+++ frontend/app/(root)/transactions/page.tsx
@@ -6,11 +6,11 @@ import { TransactionTable } from "@/app/components/transactions/TransactionsTabl
 import { Button } from "@/components/ui/button"; // Assuming you're using shadcn
 import {
 	BadgeDollarSign,
-	CalendarIcon,
 	ChevronDown,
 	ChevronUp,
 	Landmark,
 	Scale,
+	SlidersHorizontal,
 } from "lucide-react";
 import TransactionsInfoCards from "@/app/components/transactions/TransactionsInfoCards";
 import { categories } from "@/app/components/budget/CategoryComboBox";
@@ -22,19 +22,12 @@ import {
 	SelectValue,
 } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from "@/components/ui/popover";
-import { cn } from "@/lib/utils";
-import { format } from "date-fns";
-import { Calendar } from "@/components/ui/calendar";
 
 const Transactions = () => {
 	const [showFilters, setShowFilters] = useState(false);
 	const [date, setDate] = useState<Date>();
 	const [categoryFilter, setCategoryFilter] = useState<string>("");
+	const [typeFilter, setTypeFilter] = useState<string>("");
 	const [amountFilter, setAmountFilter] = useState<number | null>(null);
 
 	return (
@@ -76,26 +69,50 @@ const Transactions = () => {
 					<Button
 						variant="ghost"
 						onClick={() => setShowFilters(!showFilters)}
-						className="flex items-center text-primary hover:text-primary hover:bg-accent/25"
+						className="hidden sm:flex items-center gap-2 text-primary hover:text-primary hover:bg-accent/25"
 					>
 						{showFilters ? "Hide" : "Show"} Filters
-						{showFilters ? (
-							<ChevronUp className="ml-2" />
-						) : (
-							<ChevronDown className="ml-2" />
-						)}
+						{showFilters ? <ChevronUp /> : <ChevronDown />}
+					</Button>
+					<Button
+						variant="ghost"
+						onClick={() => setShowFilters(!showFilters)}
+						className="block sm:hidden text-primary hover:text-primary hover:bg-accent/25"
+					>
+						<SlidersHorizontal />
 					</Button>
 				</div>
 
 				<div
-					className={`grid grid-cols-3 gap-4 overflow-hidden transition-all duration-300 ${
-						showFilters ? "max-h-40 opacity-100 mt-4" : "max-h-0 opacity-0 mt-0"
+					className={`grid grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] gap-4 transition-all duration-300 ${
+						showFilters ? "max-h-60 opacity-100 mt-4" : "max-h-0 opacity-0 mt-0"
 					}`}
 				>
 					<input
 						type="date"
 						className="p-2 w-full border rounded-md bg-white text-gray-700 focus:outline-none focus:border-gray-500"
 					/>
+
+					<Select onValueChange={(value) => setTypeFilter(value)}>
+						<SelectTrigger
+							className={`p-2 h-12 border rounded-md w-full ${
+								typeFilter !== "" ? "text-black" : "text-gray-500"
+							}`}
+						>
+							<SelectValue placeholder="Select a type" />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value={"all"} className="focus:bg-accent/25">
+								All Types
+							</SelectItem>
+							<SelectItem value={"income"} className="focus:bg-accent/25">
+								Income
+							</SelectItem>
+							<SelectItem value={"expense"} className="focus:bg-accent/25">
+								Expense
+							</SelectItem>
+						</SelectContent>
+					</Select>
 
 					<Select onValueChange={(value) => setCategoryFilter(value)}>
 						<SelectTrigger
